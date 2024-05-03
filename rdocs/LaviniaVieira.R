ffa75d76b57c8b89dd11306ce9657fb6028cacc7
lavinia <buenolaviniaest@gmail.com>
2024-05-02 22:22:25 -0300

    Análise 2

--- rdocs/LaviniaVieira.R
+++ rdocs/LaviniaVieira.R
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 #___________________ Projeto Fantasma ___________________#
 #___________________ Lavinia ___________________#
 
@@ -63,20 +62,98 @@ temp_nota <- banco_final %>%
   rename("Temporada" = season, "IMDB" = imdb) %>%
   select(Temporada, IMDB) %>%
   mutate(
-    Media = round(mean(IMDB), digits = 2),
     Variancia = round(var(IMDB), digits = 2))
-
-temp_nota$Media <- str_replace(temp_nota$Media, "\\.", ",")
-temp_nota$IMDB <- str_replace(temp_nota$IMDB, "\\.", ",")
 
 view(temp_nota)
 
-fruits <- c("one apple", "two pears", "three bananas")
-str_replace_all(fruits, "[aeiou]", "-")
+#______ Boxplot 
 
-#______ 
+ggplot(temp_nota) +
+  aes(x = reorder(Temporada, IMDB, FUN = median), y = IMDB) +
+  geom_boxplot(fill = c("#A11D21"), width = 0.5) +
+  stat_summary(
+    fun = "mean", geom = "point", shape = 23, size = 3, fill = "white"
+  ) +
+  labs(x = "Temporada", y = "Nota IMDB") +
+  estat_theme()
+ggsave("boxA2.pdf", path = ("D:/documentos/lavinia/PS/PF/Projeto_Fantasma/resultados"), width = 158, height = 93, units = "mm")
 
+#______ Quadro resumo
 
+print_quadro_resumo <- function(data, title="Medidas resumo da(o) [nome da variável]", label="quad:quadro_resumo1")
+{
+  data <- data %>%
+    summarize(`Média` = round(mean(displ),2),
+              `Desvio Padrão` = round(sd(displ),2),
+              `Variância` = round(var(displ),2),
+              `Mínimo` = round(min(displ),2),
+              `1º Quartil` = round(quantile(displ, probs = .25),2),
+              `Mediana` = round(quantile(displ, probs = .5),2),
+              `3º Quartil` = round(quantile(displ, probs = .75),2),
+              `Máximo` = round(max(displ),2)) %>%
+    t() %>% 
+    as.data.frame() %>%
+    rownames_to_column()
+  
+  latex <- str_c("\\begin{quadro}[H]
+\t\\caption{", title, "}
+\t\\centering
+\t\\begin{adjustbox}{max width=\\textwidth}
+\t\\begin{tabular}{", sep="")
+  
+  col_count <- ncol(data)
+  row_count <- nrow(data)
+  latex <- str_c(latex, "| l", "|", sep=" ")
+  for (i in seq(2, col_count))
+  {
+    latex <- str_c(latex, "S", sep=" ")
+  }
+  
+  latex <- str_c(latex, "|}\n\t\\toprule\n\t\t", sep="")
+  
+  if (col_count > 2)
+  {
+    for (i in seq(1,col_count))
+    {
+      if (i == 1)
+        latex <- str_c(latex, "\\textbf{Estatística}", sep="")
+      else
+        latex <- str_c(latex, " \\textbf{", data[1, i], "}", sep="")
+      
+      if (i < col_count)
+        latex <- str_c(latex, "&", sep=" ")
+      else
+        latex <- str_c(latex, "\\\\\n", sep=" ")
+    }
+  }
+  else
+  {
+    latex <- str_c(latex, "\\textbf{Estatística} & \\textbf{Valor} \\\\\n", sep="")  
+  }
+  
+  latex <- str_c(latex, "\t\t\\midrule\n", sep="")
+  
+  if (col_count > 2)
+    starting_number <- 2
+  else
+    starting_number <- 1
+  
+  for (i in seq(starting_number, row_count))
+  {
+    latex <- str_c(latex, "\t\t", str_flatten(t(data[i,]), collapse = " & "), " \\\\\n")
+  }
+  latex <- str_c(latex, "\t\\bottomrule
+\t\\end{tabular}
+\t\\label{", label, "}
+\t\\end{adjustbox}
+\\end{quadro}", sep="")
+  
+  writeLines(latex)
+}
+
+temp_nota %>%
+  group_by(Temporada) %>%
+  print_quadro_resumo()
 
 #______ Remove ----
   
@@ -88,85 +165,5 @@ rm()
   mutate(
     freq_relativa = round(Total / sum(Total) * 100,1)
   )
-=======
-#___________________ Projeto Fantasma ___________________#
-#___________________ Lavinia ___________________#
 
-#______ Pacotes ----
-
-pacman::p_load(tidyverse, readr, readxl, lubridate, janitor)
-
-#______ Diretorio de trabalho ----
-
-setwd('D:/Documentos/lavinia/PS/PF/Projeto_Fantasma/rdocs') #getwd() retorna o diretorio atual
-
-#______ Banco de dados ----
-
-banco_final <- read_csv("D:/Documentos/lavinia/PS/PF/Projeto_Fantasma/banco/banco_final.csv")
-View(banco_final)
-
-#______ Analise 1 ----
-
-#______ Organizacao do banco de dados ----
-
-formato_data <- banco_final %>%
-  select(format, date_aired) %>%
-  rename("Decada" = date_aired, "Formato" = format) %>%
-  mutate(
-    Formato = case_when(
-    Formato == ("CrossOver") ~ "CrossOver",
-    Formato == ("Movie") ~ "Filme",
-    Formato == ("Serie") ~ "Serie"))
-
-formato_data$Decada <- formato_data$Decada %>%
-  floor_date(unit = c("10 years")) %>%
-  format("%Y") 
-
-formato_data <- formato_data %>%
-  group_by(Formato, Decada) %>%
-  arrange(Formato, Decada, sort = T, .by_group = F) %>%
-  summarise(Total = n()) %>%
-  mutate(
-    freq_relativa = round(Total / sum(Total) * 100,1)
-  )
-
-# formatoData <- with(formato_data, table(Formato, Decada)) - Table com colunas logicas
-
-#______ Graficos colunas freq relativa ----
-
-porcentagens <- str_c(formato_data$freq_relativa, "%") %>%
-  str_replace("\\.", ",")
-
-legendas <- str_squish(str_c(porcentagens))
-
-ggplot(formato_data) +
-  aes(x = fct_reorder(Formato, Total, .desc = T), y = freq_relativa,
-      fill = Decada, label = legendas) +
-  geom_col(position = position_dodge2(preserve = "single", padding = 0)) +
-  geom_text(position = position_dodge(width = .9),
-            vjust = -0.5, hjust = 0.5, size = 3) +
-  labs(x = "Formato", y = "Frequencia relativa") +
-  estat_theme()
-ggsave("graficoColFreqRelA1.pdf", plot = last_plot(), device = "pdf",
-       path = "D:/Documentos/lavinia/PS/PF/Projeto_Fantasma/resultados",
-       width = 158, height = 93, units = "mm")
-
-
-#_____ Boxplot ----
-
-ggplot(formato_data) +
-  aes(x = reorder(Formato, Total, FUN = median), y = Total) +
-  geom_boxplot(fill = c("#A11D21"), width = 0.5) +
-  stat_summary(
-    fun = "mean", geom = "point", shape = 23, size = 3, fill = "white") +
-  labs(x = "Formato", y = "Total de lancamentos") +
-  estat_theme()
-ggsave("boxFormatoData.pdf", plot = last_plot(), device = "pdf",
-       path = "D:/Documentos/lavinia/PS/PF/Projeto_Fantasma/resultados",
-       width = 158, height = 93, units = "mm")
-
-
-#______ Remove ----
-  
-rm()
->>>>>>> 29138c2a1c72527b0dafb7c3d40ec8458d5748cb
+# temp_nota$Variancia <- str_replace(temp_nota$Variancia, "\\.", ",")
